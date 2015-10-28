ba1e127a989cb11f28474d199349129705d647c0
Vikas Bharti <vikasbharti@Vikass-MacBook-Pro.local>
2015-10-28 14:28:03 +0530

    Basic Recommendation Engine

---
+++ volume1/sqlreader.py
@@ -0,0 +1,103 @@
+import MySQLdb
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+
+class DataCleaner:
+    def __init__(self):
+        self.aminity_class = self.aminites_class_reader()
+        self.workable_data, self.project_city = self.get_workable_data()
+        
+    def aminites_class_reader(self):
+        fil = open('aminities_class.txt').read().strip().split('\n')
+        aminity_class = {}
+        for line in fil:
+            line = line.split('|')
+            aminity_class[line[0].lower().strip()] = line[1].lower().strip()
+        return aminity_class
+
+    def aminities_cleaner(self, text):
+        if text:
+            text = text.split(',')
+            text = [x.strip().lower() for x in text if len(x)>1]
+            return text
+        else:
+            return []
+
+    def connect_data(self):
+        data_dict = {}
+        project_id_dict = {}
+        project_city_dict = {}
+        aminitie = []
+        category = []
+        db = MySQLdb.connect(host="127.0.0.1", port=3306, user="root", db="hdfcred")
+        cur = db.cursor()
+        #cur.execute("select Project_config_No, Project_City_Name, Map_Latitude, Map_Longitude, Config_Type, Built_Up_Area, No_Of_Balconies, No_Of_floors, No_Of_Bedroom, No_Of_Bathroom, No_Of_Units_available, Minimum_Price, Category, PricePerUnit, amenities from REDADMIN2.all_project_info")
+        cur.execute("select Project_config_No, Project_City_Name, Map_Latitude, Map_Longitude, Built_Up_Area, No_Of_Balconies, No_Of_floors, No_Of_Bedroom, No_Of_Bathroom, No_Of_Units_available, Minimum_Price, Category, PricePerUnit, amenities from REDADMIN2.all_project_info")
+        for row in cur.fetchall():
+            if not data_dict.has_key(row[1]):
+                data_dict[row[1]] = {'project_id':[], 'attributes':[]}
+            aminities = row[-1]
+            category.append(row[-3])
+            aminitie = self.aminities_cleaner(aminities)
+            class_list = [self.aminity_class.get(x, '') for x in aminitie]
+            class_list = list(set(class_list))
+            class_list = [x for x in class_list if x != '']
+            aminities_class_list =['garden', 'gym', 'outdoor sports', 'swimming pool', 'vastu', 'recreational activities', 'parking', 'health care', 'gas pipelines']
+            row = list(row[:-2])
+            #if row[-1] == '':
+            #    row[-1] = 'NONE'
+            for aminity in aminities_class_list:
+                if aminity in class_list:
+                    row.append(1)
+                else:
+                    row.append(0)
+            project_city_dict[row[0]] = row[1]
+            data_dict[row[1]]['project_id'].append(row[0])
+            r = []
+            for val in row[2:]:
+                if val in ['', 'NONE']:
+                    r.append(0.0)
+                elif val == 'AFFORDABLE':
+                    r.append(1.0)
+                elif val == 'MID_RANGE':
+                    r.append(2.0)
+                elif val == 'MID_RANGE':
+                    r.append(3.0)
+                else:
+                    try:
+                        r.append(float(val))
+                    except:
+                        r.append(0.0)
+            data_dict[row[1]]['attributes'].append(r)
+        print list(set(category))
+        return data_dict, project_city_dict
+
+    def get_workable_data(self):
+        organised_data, project_city_dict = self.connect_data()
+        for city in organised_data:
+            x = np.array(organised_data[city]['attributes']).astype(np.float)
+            x_normed = (x - x.min(axis=0))/(x.max(axis=0)-x.min(axis=0))
+            organised_data[city]['attributes'] = x_normed
+        return organised_data, project_city_dict
+
+    def simple_knn_recommender(self, project_config_No):
+        #city = self.project_city(project_config_No)
+        city = 'Mumbai'
+        X = self.workable_data[city]['attributes']
+        nbrs = NearestNeighbors(n_neighbors=11, algorithm='ball_tree').fit(X)
+        distances, indices = nbrs.kneighbors(X)
+        recomendations = {}
+        for row in indices:
+            recomendations[self.workable_data[city]['project_id'][row[0]]] = [self.workable_data[city]['project_id'][x] for x in row[1:]]
+        for row in recomendations:
+            print row, recomendations[row]
+        return recomendations
+
+
+if __name__ == '__main__':
+    mum = []
+    DC = DataCleaner()
+    DC.simple_knn_recommender(10)
+    #for ele in DC.workable_data:
+    #    if ele == 'Mumbai':
+    #        print DC.workable_data[ele]
