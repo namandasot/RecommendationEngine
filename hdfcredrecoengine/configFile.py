3a761d7a7dcbe527b13be30d6209844435f95720
naman.dasot@hdfcred.com <naman.dasot@hdfcred.com>
2017-01-13 11:06:32 +0530

    Adding config file

---
+++ hdfcredrecoengine/configFile.py
@@ -0,0 +1,5 @@
+
+#To be used to change inputs to our requirements.
+cityDict = {'1':'Mumbai','3':'Bangalore','4':'Hyderabad','6':'Chennai','7':'Delhi','10':'Pune','11':'Nashik','13':'Aurangabad','14':'Meerut','15':'Mysore','16':'Agra','17':'Ahmedabad','20':'Lucknow','21':'Kanpur','23':'Kolkata','26':'Bhubaneswar','30':'Vadodara','31':'Jaipur','32':'Indore','38':'Durgapur','39':'Bhopal','40':'Guwahati','41':'Chandigarh','71':'Jamshedpur','77':'Mangalore','136':'Jodhpur','169':'Siliguri','170':'Kerala','172':'Karnal','175':'Chikkamagaluru'}
+possessionDict = {0:1,1:75,2:150,3:320,4:640,5:980,6:1280}
+pastRadius = 8 #8km
